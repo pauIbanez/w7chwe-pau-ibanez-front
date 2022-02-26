ea9f4967f6eb0c2aa09c487584bee60e7b4d0fd5
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-26 19:46:13 +0100

    added base for token validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
+import { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const token = localStorage.getItem("token");
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const validate = () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(false);
+      }, 3000);
+    });
+
+  useEffect(
+    () =>
+      (async () => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          setAuthenticated(false);
+          setLoading(false);
+          return;
+        }
+        const valid = await validate();
+        setAuthenticated(valid);
+        setLoading(false);
+      })(),
+    []
+  );
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-
-      <Route path="/login" element={"login placeholder"} />
-      <Route path="/register" element={"register placeholder"} />
-
-      {token && <Route path="/home" element={"home placeholder"} />}
-      <Route path="/users/:id" element={"userDetails placeholder"} />
-      {token ? (
-        <Route path="*" element={"notFound placeholder"} />
+    <>
+      {loading ? (
+        <h1>LOADING</h1>
       ) : (
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+
+          <Route path="/login" element={"login placeholder"} />
+          <Route path="/register" element={"register placeholder"} />
+
+          {authenticated && <Route path="/home" element={"home placeholder"} />}
+          <Route path="/users/:id" element={"userDetails placeholder"} />
+          {authenticated ? (
+            <Route path="*" element={"notFound placeholder"} />
+          ) : (
+            <Route path="*" element={<Navigate to="/login" />} />
+          )}
+        </Routes>
       )}
-    </Routes>
+    </>
   );
 }
 

7dbe4ae25ce6ec0b38ab7176ce6fe15e7d97016b
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-26 18:49:08 +0100

    reseted react demo and added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/login" element={"login placeholder"} />
+      <Route path="/register" element={"register placeholder"} />
+      <Route path="/home" element={"home placeholder"} />
+      <Route path="/users/:id" element={"userDetails placeholder"} />
+      <Route path="*" element={"notFound placeholder"} />
+    </Routes>
   );
 }
 
